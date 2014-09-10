9a50f1f958a4f04acc2de002143f82a382731e01
Toby Inkster <mail@tobyinkster.co.uk>
2014-09-10 23:27:03 +0100

    bump version; changelog

--- lib/package/compute.pm
+++ lib/package/compute.pm
@@ -6,7 +6,7 @@ use warnings;
 
 BEGIN {
 	$package::compute::AUTHORITY = 'cpan:TOBYINK';
-	$package::compute::VERSION   = '0.004';
+	$package::compute::VERSION   = '0.005';
 }
 
 use B::Hooks::Parser 0.08 qw();
